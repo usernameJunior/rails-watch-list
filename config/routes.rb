f76e9ec27f9cb2a3587b1937cab2d7a63547678a
Olivier Genel <zotri@hotmail.com>
2022-11-17 16:54:47 +0100

    controllers done

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,19 @@
 Rails.application.routes.draw do
+  # get 'bookmarks/index'
+  # get 'bookmarks/show'
+  # get 'bookmarks/new'
+  # get 'bookmarks/create'
+  # get 'lists/index'
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
   # root "articles#index"
+
+  root 'lists#index'
+
+  resources :lists, only: %i[index show new create] do
+    resources :bookmarks, only: %i[show new create]
+  end
+
+  resources :bookmarks, only: %i[destroy]
 end
